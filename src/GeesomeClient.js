dff50942926933a9df800a242ef00d8bf242b8c0
jonybang <Jonybange@gmail.com>
2019-08-17 01:26:22 +0200

    getNodeAddress

--- src/GeesomeClient.js
+++ src/GeesomeClient.js
@@ -525,9 +525,13 @@ class GeesomeClient {
     let addresses = await this.getNodeAddressList();
 
     if(includes) {
-      return _.find(addresses, (address) => {
+      let address = _.find(addresses, (address) => {
         return _.includes(address, includes);
       });
+      if(this.isLocalServer()) {
+        address = address.replace('4002/ipfs', '4003/ws/ipfs')
+      }
+      return address;
     } else {
       return _.filter(addresses, (address) => {
         return !_.includes(address, '127.0.0.1') && !_.includes(address, '192.168') && address.length > 64;//&& !_.includes(address, '/p2p-circuit/ipfs/')
@@ -540,7 +544,7 @@ class GeesomeClient {
   }
 
   async connectToIpfsNodeToServer() {
-    const address = await this.getNodeAddress(this.isLocalServer() ? '/p2p-circuit/ipfs/' : null);
+    const address = await this.getNodeAddress(this.isLocalServer() ? '127.0.0.1' : null);
     
     // prevent Error: Dial is currently blacklisted for this peer on swarm connect
     this.ipfsNode.libp2p._switch.dialer.clearBlacklist(new PeerInfo(PeerId.createFromB58String(_.last(address.split('/')))));
