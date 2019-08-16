0479a3b456af5ed7f8490c94cf7e7134a539b204
jonybang <Jonybange@gmail.com>
2019-08-17 00:17:31 +0200

    rework addBootNode

--- src/JsIpfsService.js
+++ src/JsIpfsService.js
@@ -171,14 +171,15 @@ module.exports = class JsIpfsService {
   }
 
   async addBootNode(address) {
+    await new Promise((resolve, reject) => {
+      this.node.bootstrap.add(address, (err, res) => err ? reject(err) : resolve(res.Peers));
+    });
+    
     try {
       await this.swarmConnect(address);
     } catch (e) {
       console.warn('addBootNode swarm.connect error', address, e);
     }
-    return new Promise((resolve, reject) => {
-      this.node.bootstrap.add(address, (err, res) => err ? reject(err) : resolve(res.Peers));
-    });
   }
 
   async removeBootNode(address) {
