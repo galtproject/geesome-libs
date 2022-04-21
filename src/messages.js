72657ab37cf53aa74797e47ac040b313891cf901
MicrowaveDev <jonybange@gmail.com>
2022-04-21 13:58:02 +0800

    add acceptInvite message

---
+++ src/messages.js
@@ -0,0 +1,5 @@
+module.exports = {
+    acceptInvite(storageAccIpns, code) {
+        return 'I accept the invitation to the Geesome node with storage account: ' + storageAccIpns + ' and code:' + code;
+    }
+}
